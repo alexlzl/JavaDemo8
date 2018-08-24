21250a97bc12a903a9abafc460a7f0459644779f
liuzhouliang <525688067@qq.com>
2018-08-24 13:57:43 +0800

    注解

--- 父类引用指向子对象/src/com/test/Main1.java
+++ 父类引用指向子对象/src/com/test/Main1.java
@@ -7,6 +7,7 @@ public class Main1 {
     public static void main(String [] args) {
         B1 b = new B1();
         A1 a = (A1) b;
+        A1 aa=new A1();
 
         System.out.println(b.getClass());//class com.test.B1
         System.out.println(a.getClass());//class com.test.B1
@@ -15,6 +16,7 @@ public class Main1 {
         System.out.println(b instanceof A1);//true
         System.out.println(a instanceof B1);//true
         System.out.println(b instanceof B1);//true
+        System.out.println(a instanceof A1);//true
     }
 }
 
