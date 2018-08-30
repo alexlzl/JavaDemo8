2be5b99ae9665c795f759c52eb01a5f49b5d90bb
liuzhouliang <525688067@qq.com>
2018-08-30 18:16:53 +0800

    Merge branch 'master' into Branch2
    # Conflicts:
    #	VCS1/src/com/test/Test.java

---
+++ Static块执行时机/src/com/test/B.java
@@ -0,0 +1,27 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2018/8/30.
+ * <p>
+ * 实际上，static块的执行发生在“初始化”的阶段。初始化阶段，jvm主要完成对静态变量的初始化，静态块执行等工作。
+ * <p>
+ * 下面我们看看执行static块的几种情况：
+ * <p>
+ * 1、第一次new A()的过程会打印""；因为这个过程包括了初始化
+ * <p>
+ * 2、第一次Class.forName("A")的过程会打印""；因为这个过程相当于Class.forName("A",true,this.getClass().getClassLoader());
+ * <p>
+ * 3、第一次Class.forName("A",false,this.getClass().getClassLoader())的过程则不会打印""。因为false指明了装载类的过程中，不进行初始化。不初始化则不会执行static块。
+ */
+public class B {
+    public static void main(String[] args) {
+        try {
+//            Class.forName("com.test.A",false,B.class.getClassLoader());//加载了类没有进行了初始化
+//            Class.forName("com.test.A",true,B.class.getClassLoader());//加载了类并且进行了初始化
+            Class.forName("com.test.A");//加载了类并且进行了初始化
+//            A a=new A();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
