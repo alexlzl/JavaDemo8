92cc0095d7d498baf0cc784b99cec006aca1d226
liuzhouliang <525688067@qq.com>
2018-08-31 10:04:47 +0800

    Merge branch 'master' into Branch2
    # Conflicts:
    #	VCS1/src/com/test/Test.java

--- Static变量，块执行时机/src/com/test/B.java
+++ Static变量，块执行时机/src/com/test/B.java
@@ -17,15 +17,23 @@ public class B {
     public static void main(String[] args) {
 //        int age = A.age;
 //        int age1=A.age1;//调用静态常量不会引起类加载
-        int age2 = A.age2;
-//        try {
-////            Class.forName("com.test.A",false,B.class.getClassLoader());//加载了类没有进行了初始化
-////            Class.forName("com.test.A",true,B.class.getClassLoader());//加载了类并且进行了初始化
+//        int age2 = A.age2;
+        try {
+        Class<A> aClass= (Class<A>) Class.forName("com.test.A",false,B.class.getClassLoader());//加载了类没有进行了初始化
+//            try {
+//                A a=aClass.newInstance();
+//                System.out.println("静态变量未初始化=========="+a.age1);
+//            } catch (InstantiationException e) {
+//                e.printStackTrace();
+//            } catch (IllegalAccessException e) {
+//                e.printStackTrace();
+//            }
+//            Class.forName("com.test.A",true,B.class.getClassLoader());//加载了类并且进行了初始化
 //            Class.forName("com.test.A");//加载了类并且进行了初始化
-////            A a=new A();
-//
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
+//            A a=new A();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
