c074f90b2d8020516e10da12bcd571b5f8c3539f
liuzhouliang <525688067@qq.com>
2018-11-29 10:29:26 +0800

    java8

---
+++ Demo1/src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Test {
+
+    public static void test(){
+         Inn.getName();
+    }
+
+    public static class Inn{
+        public static final void getName(){
+//            test();
+        }
+    }
+}
